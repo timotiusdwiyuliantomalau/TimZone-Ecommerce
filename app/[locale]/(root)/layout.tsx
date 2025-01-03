4c605cb07e2f1c0cbd3addb5b15e2a40dca5afea
timotiusdwiyuliantomalau <timotiusdym17@gmail.com>
2025-01-04 00:20:04 +0700

    Update

--- app/[locale]/(root)/layout.tsx
+++ app/[locale]/(root)/layout.tsx
@@ -2,17 +2,17 @@ import React from 'react'
 
 import Header from '@/components/shared/header'
 import Footer from '@/components/shared/footer'
-
-export default async function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return (
-    <div className='flex flex-col min-h-screen'>
-      <Header />
-      <main className='flex-1 flex flex-col p-4'>{children}</main>
-      <Footer />
-    </div>
-  )
+export default function RootErrorBoundary({ children }:any) {
+  try {
+    return (
+      <div className='flex flex-col min-h-screen'>
+        <Header />
+        <main className='flex-1 flex flex-col p-4'>{children}</main>
+        <Footer />
+      </div>
+    )
+  } catch (error) {
+    console.error(error); 
+    return <div>Something went wrong</div>;
+  }
 }
