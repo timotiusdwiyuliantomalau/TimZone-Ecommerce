60f60fb14c81ee85e5457ed69c9367d087bf72ae
timotiusdwiyuliantomalau <timotiusdym17@gmail.com>
2025-01-03 23:47:09 +0700

    Update

--- app/[locale]/loading.tsx
+++ app/[locale]/loading.tsx
@@ -1,10 +1,9 @@
-import { getTranslations } from "next-intl/server";
+import Image from "next/image";
 
 export default async function LoadingPage() {
-  const t = await getTranslations();
   return (
     <div className="flex flex-col items-center justify-center min-h-screen ">
-      <img className="w-[15em]" src="/icons/loading.gif" alt="" />
+      <Image className="w-[15em]" src="/icons/loading.gif" alt="" />
     </div>
   );
 }
--- components/shared/footer.tsx
+++ components/shared/footer.tsx
@@ -1,111 +1,106 @@
-'use client'
-import { ChevronUp } from 'lucide-react'
-import Link from 'next/link'
-import Image from 'next/image'
-
-import { Button } from '@/components/ui/button'
-import useSettingStore from '@/hooks/use-setting-store'
-import { Select, SelectContent, SelectItem, SelectTrigger } from '../ui/select'
-
-import { SelectValue } from '@radix-ui/react-select'
-import { useLocale, useTranslations } from 'next-intl'
-import { usePathname, useRouter } from '@/i18n/routing'
-import { i18n } from '@/i18n-config'
+"use client";
+import { ChevronUp } from "lucide-react";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import useSettingStore from "@/hooks/use-setting-store";
+import { useLocale, useTranslations } from "next-intl";
+import { usePathname, useRouter } from "@/i18n/routing";
+import { i18n } from "@/i18n-config";
 
 export default function Footer() {
-  const router = useRouter()
-  const pathname = usePathname()
+  const router = useRouter();
+  const pathname = usePathname();
   const {
     setting: { site, availableCurrencies, currency },
     setCurrency,
-  } = useSettingStore()
-  const { locales } = i18n
+  } = useSettingStore();
+  const { locales } = i18n;
 
-  const locale = useLocale()
-  const t = useTranslations()
+  const locale = useLocale();
+  const t = useTranslations();
   return (
-    <footer className='bg-black  text-white underline-link'>
-      <div className='w-full'>
+    <footer className="bg-black  text-white underline-link">
+      <div className="w-full">
         <Button
-          variant='ghost'
-          className='bg-gray-800 w-full  rounded-none '
-          onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
+          variant="ghost"
+          className="bg-gray-800 w-full  rounded-none "
+          onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
         >
-          <ChevronUp className='mr-2 h-4 w-4' />
-          {t('Footer.Back to top')}
+          <ChevronUp className="mr-2 h-4 w-4" />
+          {t("Footer.Back to top")}
         </Button>
-        <div className='grid grid-cols-1 md:grid-cols-3 gap-6 p-6 max-w-7xl mx-auto'>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6 p-6 max-w-7xl mx-auto">
           <div>
-            <h3 className='font-bold mb-2'>{t('Footer.Get to Know Us')}</h3>
-            <ul className='space-y-2'>
+            <h3 className="font-bold mb-2">{t("Footer.Get to Know Us")}</h3>
+            <ul className="space-y-2">
               <li>
-                <Link href='/page/careers'>{t('Footer.Careers')}</Link>
+                <Link href="/page/careers">{t("Footer.Careers")}</Link>
               </li>
               <li>
-                <Link href='/page/blog'>{t('Footer.Blog')}</Link>
+                <Link href="/page/blog">{t("Footer.Blog")}</Link>
               </li>
               <li>
-                <Link href='/page/about-us'>
-                  {t('Footer.About name', { name: "TimZone" })}
+                <Link href="/page/about-us">
+                  {t("Footer.About name", { name: "TimZone" })}
                 </Link>
               </li>
             </ul>
           </div>
           <div>
-            <h3 className='font-bold mb-2'>{t('Footer.Make Money with Us')}</h3>
-            <ul className='space-y-2'>
+            <h3 className="font-bold mb-2">{t("Footer.Make Money with Us")}</h3>
+            <ul className="space-y-2">
               <li>
-                <Link href='/page/sell'>
-                  {t('Footer.Sell products on', { name: "TimZone" })}
+                <Link href="/page/sell">
+                  {t("Footer.Sell products on", { name: "TimZone" })}
                 </Link>
               </li>
               <li>
-                <Link href='/page/become-affiliate'>
-                  {t('Footer.Become an Affiliate')}
+                <Link href="/page/become-affiliate">
+                  {t("Footer.Become an Affiliate")}
                 </Link>
               </li>
               <li>
-                <Link href='/page/advertise'>
-                  {t('Footer.Advertise Your Products')}
+                <Link href="/page/advertise">
+                  {t("Footer.Advertise Your Products")}
                 </Link>
               </li>
             </ul>
           </div>
           <div>
-            <h3 className='font-bold mb-2'>{t('Footer.Let Us Help You')}</h3>
-            <ul className='space-y-2'>
+            <h3 className="font-bold mb-2">{t("Footer.Let Us Help You")}</h3>
+            <ul className="space-y-2">
               <li>
-                <Link href='/page/shipping'>
-                  {t('Footer.Shipping Rates & Policies')}
+                <Link href="/page/shipping">
+                  {t("Footer.Shipping Rates & Policies")}
                 </Link>
               </li>
               <li>
-                <Link href='/page/returns-policy'>
-                  {t('Footer.Returns & Replacements')}
+                <Link href="/page/returns-policy">
+                  {t("Footer.Returns & Replacements")}
                 </Link>
               </li>
               <li>
-                <Link href='/page/help'>{t('Footer.Help')}</Link>
+                <Link href="/page/help">{t("Footer.Help")}</Link>
               </li>
             </ul>
           </div>
         </div>
       </div>
-      <div className='p-4'>
-        <div className='flex justify-center  gap-3 text-sm'>
-          <Link href='/page/conditions-of-use'>
-            {t('Footer.Conditions of Use')}
+      <div className="p-4">
+        <div className="flex justify-center  gap-3 text-sm">
+          <Link href="/page/conditions-of-use">
+            {t("Footer.Conditions of Use")}
           </Link>
-          <Link href='/page/privacy-policy'>{t('Footer.Privacy Notice')}</Link>
-          <Link href='/page/help'>{t('Footer.Help')}</Link>
+          <Link href="/page/privacy-policy">{t("Footer.Privacy Notice")}</Link>
+          <Link href="/page/help">{t("Footer.Help")}</Link>
         </div>
-        <div className='flex justify-center text-sm'>
+        <div className="flex justify-center text-sm">
           <p> © Timotius Malau | 2024</p>
         </div>
-        <div className='mt-8 flex justify-center text-sm text-gray-400'>
+        <div className="mt-8 flex justify-center text-sm text-gray-400">
           Indonesia | +6280233243
         </div>
       </div>
     </footer>
-  )
+  );
 }
